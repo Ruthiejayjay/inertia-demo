5c1e7cb96886ac0399572f84527aeeb1790a0400
Ruth James <jamesruth449@gmail.com>
2023-04-26 19:36:38 +0100

    added defualt layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,10 @@
 <template>
-    <Layout>
-        <h1 class="text-3xl font-bold">Hello World</h1>
-    </Layout>
-    
+    <h1 class="text-3xl font-bold">Hello World</h1>
 </template>
 
 <script>
 import Layout from '../Shared/Layout.vue';
 export default {
-    components: {
-        Layout,
-    },
+    layout: Layout
 }
 </script>
--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,21 +1,15 @@
 <template>
-    <Layout>
-        <h1 class="text-3xl font-bold">Users</h1>
-        <div style="margin-top: 400px;">
-            <p>The current time is {{ time }}.</p>
-            <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
-        </div>
-    </Layout>
+    <h1 class="text-3xl font-bold">Users</h1>
+    <div style="margin-top: 400px;">
+        <p>The current time is {{ time }}.</p>
+        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+    </div>
 </template>
 
 <script>
 import Layout from '../Shared/Layout.vue';
-import { Link } from '@inertiajs/inertia-vue3'
 export default {
-    components: {
-        Layout,
-        Link
-    },
+    layout: Layout,
     props: { time: String }
 }
 </script>
