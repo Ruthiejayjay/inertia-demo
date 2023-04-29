30924c953bd8b31cc9666db34a1e548fd72874fd
Ruth James <jamesruth449@gmail.com>
2023-04-29 19:02:00 +0100

    completed form

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -22,24 +22,31 @@
                 Password
             </label>
             <input v-model="form.password" type="password" id="password" name="password" class="border border-gray-400 p-2 w-full" required>
-            <div v-if="$page.props.errors.password" v-text="$page.props.errors.password" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
         <div class="mb-6">
-            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
+            <button 
+                type="submit" 
+                class="
+                bg-blue-400 
+                text-white 
+                rounded py-2
+                 px-4 hover:bg-blue-500" 
+                 :disabled="form.processing">
+                 Submit</button>
         </div>
     </form>
 </template>
 
 <script setup>
-    import { reactive } from 'vue'
-    import { Inertia } from '@inertiajs/inertia'
-    let form = reactive({
+    import { useForm } from '@inertiajs/inertia-vue3';
+    let form = useForm({
         name: '',
         email: '',
         password: ''
     });
 
     let submit = () => {
-        Inertia.post('/users', form);
+        form.post('/users');
     }
 </script>
